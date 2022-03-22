b37e80040f0cb8ff08a526219109262ba6635473
Nyk Ma <nykma@mask.io>
2022-03-22 19:39:07 +0800

    [#] examples/lambda: init env_logger

--- examples/lambda.rs
+++ examples/lambda.rs
@@ -3,6 +3,7 @@ use lambda_http::{service_fn, Error as LambdaError};
 
 #[tokio::main]
 async fn main() -> Result<(), LambdaError> {
+    let _ = env_logger::try_init();
     lambda_http::run(service_fn(entrypoint)).await?;
     Ok(())
 }
