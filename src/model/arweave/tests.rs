f7b1477397423f8331abc7d93c275ad2c379733f
lyfer233 <xiaofanhengeili@gmail.com>
2023-09-15 03:35:43 +0000

    delete a unit test

--- src/model/arweave/tests.rs
+++ src/model/arweave/tests.rs
@@ -58,42 +58,4 @@ mod tests {
         assert!(status_code == 200 || status_code == 202);
     }
 
-    /// Check the upload data on arweave is correct or not
-    // #[tokio::test]
-    // async fn test_check_data() {
-    //     use std::thread::sleep;
-    //     use std::time::Duration;
-    //     use http::Uri;
-    //     use hyper::Client;
-    //     use hyper_tls::HttpsConnector;
-
-    //     let upload_document = prepare_test_document();
-    //     let transcation_id = upload_document.clone().upload_to_arweave().await.unwrap();
-    //     // let transcation_id = "lB9_qabnusXeunR57Qk8oP9_-pPkEkoajNGTOlj082Y".to_string();
-    //     let url = format!("https://arweave.net/tx/{}/data", transcation_id).parse::<Uri>().unwrap();
-
-    //     let https = HttpsConnector::new();
-    //     let client = Client::builder().build::<_, hyper::Body>(https);
-    //     loop {
-    //         match client.get(url.clone()).await {
-    //             Ok(mut resp) => {
-    //                 if resp.status() == 200 {
-    //                     let body = hyper::body::to_bytes(resp.body_mut()).await.unwrap();
-    //                     let encode_str = std::str::from_utf8(&body).unwrap().to_string();
-    //                     let decoded = base64::decode(encode_str).unwrap();
-    //                     assert_eq!(decoded, serde_json::to_vec(&upload_document).unwrap());
-    //                     break;
-    //                 } else if resp.status().is_server_error() {
-    //                     println!("Server error!");
-    //                 } else {
-    //                     println!("Received response: {:?}", resp.status());
-    //                 }
-    //             }
-    //             Err(e) => println!("Request failed: {:?}", e),
-    //         }
-    //         sleep(Duration::from_secs(2));
-    //     }
-    // }
-
-
 }
