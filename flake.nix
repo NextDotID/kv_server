93e62798969b2ffb0a4d441fb053057b45e42191
Nyk Ma <nykma@mask.io>
2023-10-26 02:32:27 +0800

    [+][DEV] nix: dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    # Cannot using naersk due to lack of `arweave-rs` package built
+    # naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        # defaultPackage = naersk-lib.buildPackage ./.;
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ cargo rustc rustfmt rust-analyzer pre-commit rustPackages.clippy pkg-config openssl postgresql ];
+          RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        };
+      });
+}
